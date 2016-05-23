c41ec76cd48c948c4bb27f28e4a182296936a5a7
Ben Rachbach <brachbach@gmail.com>
2016-05-23 11:43:43 -0700

    Attempting hello world, testing pomander

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 angular.module('farmSanctuary', [ui.router])
-.config(function()){
+.config(function ($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/ateToday');
   $stateProvider
     .state('ateToday', {
       url: '/ateToday',
       templateUrl: 'app/ateToday.html',
       controller: 'ateTodayController'
-    })
-}
+    });
+});
